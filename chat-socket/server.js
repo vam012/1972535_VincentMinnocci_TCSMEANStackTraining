7e619e489236a5aec0a6369bc6424a8ef16592a3
Vincent Minnocci <vincentminnocci@gmail.com>
2021-04-12 14:23:51 -0400

    added dead client connection fix

--- chat-socket/server.js
+++ chat-socket/server.js
@@ -4,6 +4,7 @@ let io = require("socket.io")(http);
 const port = 9090;
 let clientNum = 0;
 let connectionList = new Array();
+let removeList = new Array();
 
 app.get("/",(req,res)=>{
     res.sendFile(__dirname+"/index.html");
@@ -16,9 +17,14 @@ io.on("connection",(socket)=>{
     console.log(`New client with client number ${clientNum-1} connected`)
     socket.on("clientMsg",(data)=>{
         console.log(`${data.name} (CLIENT NUMBER:${data.cNum}) says "${data.msg}"`);
-        connectionList.forEach(connection => {
-            connection.emit("serverMsg",data);
-        }); 
+        let length = connectionList.length
+        for (let i = 0; i < length; i++) {
+            const connection = connectionList[i];
+            if(connection.connected == false){removeList.push(i);}  //connection is dead remove
+            else{connection.emit("serverMsg",data);}                //send new message
+        }
+        removeList.forEach(indexToRemove => {connectionList.splice(indexToRemove,1);}); //remove all dead connections
+        removeList = new Array();                                                       //reset dead connections list
     }); 
 });
 
